1704df102126c30b9499d31321a2badceabffc9d
MunibBukhari <munib.bukhari@graana.com>
2023-01-03 14:08:07 +0500

    webpack configuaration add loaders and set the router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/home/Home'
 import './App.scss'
+
 const App = () => {
   return (
     <div>
